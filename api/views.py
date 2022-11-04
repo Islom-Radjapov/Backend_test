cdc9829024eb39e315e02b6f318c642397a492ec
islom <radjapov_islom@mail.ru>
2022-11-04 22:12:30 +0500

    changed views

--- api/views.py
+++ api/views.py
@@ -1,11 +1,8 @@
-import time
 from .apps import ApiConfig
-from .apps import AppConfig
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import cv2
 import cvlib as cv
-from keras.utils import img_to_array
 import numpy as np
 
 
@@ -13,10 +10,8 @@ class GenderPradict(APIView):
     def post(self, request):
         data = request.FILES
         data = data['foto']
-        print( data )
-        print( type(data) )
 
-        img = cv2.VideoCapture( data['foto'] )
+        img = cv2.VideoCapture( data )
 
         classes = ['man', 'woman']
 
@@ -27,27 +22,22 @@ class GenderPradict(APIView):
         face, confidence = cv.detect_face(frame)
 
         # loop through detected faces
-        for x, y in enumerate(face):
-            idx = x
+        for _, y in enumerate(face):
             f = y
 
         # get corner points of face rectangle
         (startX, startY) = f[0], f[1]
         (endX, endY) = f[2], f[3]
 
-        # draw rectangle over face
-        # cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 0), 2)
-
         # crop the detected face region
         face_crop = np.copy(frame[startY:endY, startX:endX])
 
-        # if (face_crop.shape[0]) < 10 or (face_crop.shape[1]) < 10:
-        #     continue
+        if (face_crop.shape[0]) < 10 or (face_crop.shape[1]) < 10:
+            return Response( "Inson suratni to'liq jonating!", status=200 )
 
         # preprocessing for gender detection model
         face_crop = cv2.resize(face_crop, (96, 96))
         face_crop = face_crop.astype("float") / 255.0
-        face_crop = img_to_array(face_crop)
         face_crop = np.expand_dims(face_crop, axis=0)
 
         # apply gender detection on face
@@ -57,8 +47,6 @@ class GenderPradict(APIView):
         idx = np.argmax(conf)
         label = classes[idx]
 
-        # label = "{}: {:.2f}%".format(label, conf[idx] * 100)
-
         response = {"Gender": label}
 
-        return Response( "response", status=200 )
+        return Response( response, status=200 )
