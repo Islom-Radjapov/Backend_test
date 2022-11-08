872e1b33749aac1d36839c21c5e398a9d75f2925
islom <radjapov_islom@mail.ru>
2022-11-08 21:28:10 +0500

    add workcpace

---
+++ workspace.py
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import datetime
+#
+#
+# new = datetime.datetime.now()
+# print(new)
+# for x in range(17000):
+#     print(x)
+# print(new)
+# print(datetime.datetime.now() - new)
+#
+
+
+
+
+# include <iostream>
+# include <ctime>
+
+# using
+# namespace
+# std;
+#
+# int
+# main()
+# {
+#     time_t
+# ttime = time(0);
+# tm * local_time = localtime( & ttime);
+# cout << "Time: " << local_time->tm_hour << ":";
+# cout << local_time->tm_min << ":";
+# cout << local_time->tm_sec << endl;
+# for (int i = 0; i < 17000; i++)
+# {
+# // cout <<;
+# }
+#
+# time_t
+# tttime = time(0);
+# tm * local_timee = localtime( & tttime);
+# cout << "Time: " << local_timee->tm_hour << ":";
+# cout << local_timee->tm_min << ":";
+# cout << local_timee->tm_sec << endl;
+#
+# return 0;
+# }
+
+
